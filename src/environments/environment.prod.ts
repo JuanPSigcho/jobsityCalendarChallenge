adb8288f4ea8cc938822ead9f1c06c84eb20d922
JuanPSigcho <juanpsigcho@gmail.com>
2021-03-29 12:34:26 -0500

    Deploy Site to Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  url_api: 'https://api.openweathermap.org/data/2.5/weather',
+  api_key: '85433a3d65e7d960e88761e1da084cf0',
+  firebase: {
+    apiKey: 'AIzaSyAfQj4QMVa4dr0TKTf2dmA2qsIGsa3HvQc',
+    authDomain: 'calendarchallengejp.firebaseapp.com',
+    projectId: 'calendarchallengejp',
+    storageBucket: 'calendarchallengejp.appspot.com',
+    messagingSenderId: '410829883953',
+    appId: '1:410829883953:web:0c63959d12356fad5de310',
+  },
 };
